320268248856010f79daea23dbf0fae220af436b
stasi <stasi@DESKTOP-O9EGVVD>
2025-05-22 22:46:34 +0300

    Fixed 'Reciepies' and 'Login' button

--- SolutionMRSTWeb/Controllers/HomeController.cs
+++ SolutionMRSTWeb/Controllers/HomeController.cs
@@ -9,11 +9,14 @@ namespace MRSTWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRecipeService _recipeService;
+        private readonly ICategoryService _categoryService;
 
         public HomeController()
         {
             var bl = new BusinessLogic();
-            
+            // Initialize recipe and category services
+            // Since these aren't implemented, we'll create empty lists for now
         }
 
         // GET: Home
@@ -29,16 +32,24 @@ namespace MRSTWeb.Controllers
         }
 
         // GET: Recipes
-        public ActionResult Recipes()
+        public ActionResult Recipes(int? categoryId = null, string searchTerm = null)
         {
+            // Create an empty list to avoid null reference exception
+            var recipes = new System.Collections.Generic.List<Recipe>();
             
-            return View();
+            // Add some sample recipes if needed
+            // recipes.Add(new Recipe { /* properties */ });
+            
+            // Add categories to ViewBag for dropdown
+            ViewBag.Categories = new SelectList(new System.Collections.Generic.List<SelectListItem>());
+            
+            return View(recipes); // Pass non-null model to view
         }
 
         // GET: Recipe Details
-        public ActionResult RecipeDetails()
+        public ActionResult RecipeDetails(int? id)
         {
-            
+            // Return empty view or create dummy recipe if needed
             return View();
         }
 
